35a670986ae48ed5540057724a26df6c1a2e3856
sumito3478 <sumito3478@gmail.com>
2013-12-15 11:42:34 +0900

    Modify the argument of DWT functions to mutable.IndexedSeq[Double]

--- src/main/scala/info/sumito3478/dwt97.scala
+++ src/main/scala/info/sumito3478/dwt97.scala
@@ -3,14 +3,15 @@ package info.sumito3478
 package object dwt97 {
   import scalaxy.loops._
   import scala.language.postfixOps
+  import scala.collection._
 
-  def fwt97(xs: Array[Float]) = {
-    val a1 = -1.586134342f
-    val a2 = -0.05298011854f
-    val a3 = 0.8829110762f
-    val a4 = 0.4435068522f
-    val k1 = 0.81289306611596146f
-    val k2 = 0.61508705245700002f
+  def fwt97(xs: mutable.IndexedSeq[Double]) = {
+    val a1 = -1.586134342
+    val a2 = -0.05298011854
+    val a3 = 0.8829110762
+    val a4 = 0.4435068522
+    val k1 = 0.81289306611596146
+    val k2 = 0.61508705245700002
     val n = xs.length
     // Predict 1
     for (i <- 1 until n - 2 by 2 optimized)
@@ -29,29 +30,33 @@ package object dwt97 {
       xs(i) += a4 * (xs(i - 1) + xs(i + 1))
     xs(0) += 2 * a4 * xs(1)
     // Scale and pack
-    val tmp = new Array[Float](n)
+    val tmp = new Array[Double](n)
     for (i <- 0 until n / 2 optimized) {
       tmp(i) = k1 * xs(i * 2)
       tmp(n / 2 + i) = k2 * xs(i * 2 + 1)
     }
-    Array.copy(tmp, 0, xs, 0, n)
+    for (i <- 0 until n optimized) {
+      xs(i) = tmp(i)
+    }
   }
 
-  def iwt97(xs: Array[Float]) = {
-    val a1 = 1.586134342f
-    val a2 = 0.05298011854f
-    val a3 = -0.8829110762f
-    val a4 = -0.4435068522f
-    val k1 = 1.230174104914f
-    val k2 = 1.6257861322319229f
+  def iwt97(xs: mutable.IndexedSeq[Double]) = {
+    val a1 = 1.586134342
+    val a2 = 0.05298011854
+    val a3 = -0.8829110762
+    val a4 = -0.4435068522
+    val k1 = 1.230174104914
+    val k2 = 1.6257861322319229
     val n = xs.length
     // Unpack and Undo Scale
-    val tmp = new Array[Float](n)
+    val tmp = new Array[Double](n)
     for (i <- 0 until n / 2 optimized) {
       tmp(i * 2) = k1 * xs(i)
       tmp(i * 2 + 1) = k2 * xs(n / 2 + i)
     }
-    Array.copy(tmp, 0, xs, 0, n)
+    for (i <- 0 until n optimized) {
+      xs(i) = tmp(i)
+    }
     // Undo update 2
     for (i <- 2 until n by 2 optimized) {
       xs(i) += a4 * (xs(i - 1) + xs(i + 1))
